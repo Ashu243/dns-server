805f6c0ee88313ea25d77e04f2803361620664c9
Ashu <ashuv6309@gmail.com>
2026-04-05 00:29:50 +0530

    fix response id mismatch

--- index.js
+++ index.js
@@ -9,36 +9,46 @@ const db = {
 
 }
 
+const cache = new Map()
+
 // receive the packet
 server.on('message', (msg, rinfo) => {
 
     // decode the message
     const incomingreq = dnsPacket.decode(msg)
-    const ipFromDb = db[incomingreq.questions[0].name]
-
-    // if (!ipFromDb) {
-    //     const notFoundResponse = dnsPacket.encode({
-    //         id: incomingreq.id,
-    //         type: 'response',
-    //         flags: dnsPacket.AUTHORITATIVE_ANSWER,
-    //         questions: incomingreq.questions,
-    //         answers: []
-    //     });
-
-    //     server.send(notFoundResponse, rinfo.port, rinfo.address);
-    //     return;
-    // }
+    const domain = incomingreq.questions[0].name
+    const ipFromDb = db[domain]
 
 
     if(!ipFromDb){
+        const cached = cache.get(domain)
+        
+        if(cached && cached.expiresAt > Date.now()){
+            const ans = dnsPacket.encode({
+                id: incomingreq.id,
+                type: 'response',
+                flags: dnsPacket.AUTHORITATIVE_ANSWER,
+                questions: incomingreq.questions,
+                answers: cached.answers
+            })
+            console.log('cached data:', cached)
+            server.send(ans, rinfo.port, rinfo.address)
+            return
+        }
+
+
         upstream.send(msg, 53, '8.8.8.8')
 
         upstream.once('message', (response)=> {
+            const decoded = dnsPacket.decode(response)
+            cache.set(domain, {
+                answers: decoded.answers,
+                expiresAt:  Date.now() + (5 * 60) * 1000
+            })
             server.send(response, rinfo.port, rinfo.address)
         })
         return
     }
-
 
 
     const ans = dnsPacket.encode({
@@ -48,7 +58,7 @@ server.on('message', (msg, rinfo) => {
         questions: incomingreq.questions,
         answers: [{
             type: 'A',
-            name: incomingreq.questions[0].name,
+            name: domain,
             class: 'IN',
             data: ipFromDb
         }]
