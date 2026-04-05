75d8aff6fa23b02a4b2a59ad3094e3608d82d5be
Ashu <ashuv6309@gmail.com>
2026-04-05 23:34:38 +0530

    add multi valued A record type

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const dnsPacket = require('dns-packet')
 const upstream = dgram.createSocket('udp4')
 
 const db = {
-    'piyushgarg.dev': '1.2.3.4',
-    'blog.piyushgarg.dev': '4.5.6.7'
+    'piyushgarg.dev': {
+        A: ['1.2.3.4', '3.4.5.6'],
+        TXT: 'hello from custom dns',
+        MX: 'mail.piyushgarg.dev'
+    },
+    'blog.piyushgarg.dev': {
+        CNAME: 'piyushgarg.dev'
+    }
 
 }
 
@@ -17,11 +23,13 @@ server.on('message', (msg, rinfo) => {
     // decode the message
     const incomingreq = dnsPacket.decode(msg)
     const domain = incomingreq.questions[0].name
-    const ipFromDb = db[domain]
+    const type = incomingreq.questions[0].type
+    const ipFromDb = db[domain]?.[type]
+    console.log(ipFromDb)
 
-
+    // forward the same to google dns server if not found in db
     if(!ipFromDb){
-        const cached = cache.get(domain)
+        const cached = cache.get(`${domain}:${type}`)
         
         if(cached && cached.expiresAt > Date.now()){
             const ans = dnsPacket.encode({
@@ -41,27 +49,29 @@ server.on('message', (msg, rinfo) => {
 
         upstream.once('message', (response)=> {
             const decoded = dnsPacket.decode(response)
-            cache.set(domain, {
+            const ttl = decoded.answers?.[0]?.ttl || 300
+            cache.set(`${domain}:${type}`, {
                 answers: decoded.answers,
-                expiresAt:  Date.now() + (5 * 60) * 1000
+                expiresAt:  Date.now() + ttl * 1000
             })
             server.send(response, rinfo.port, rinfo.address)
         })
         return
     }
 
-
+    // if not cached and my database has the record
     const ans = dnsPacket.encode({
         id: incomingreq.id,
         type: 'response',
         flags: dnsPacket.AUTHORITATIVE_ANSWER,
         questions: incomingreq.questions,
-        answers: [{
-            type: 'A',
+        answers: ipFromDb.map((ip)=>({
+            type: type,
             name: domain,
             class: 'IN',
-            data: ipFromDb
-        }]
+            ttl: 300,
+            data: ip
+    }))
     })
     console.log({
         msg: incomingreq.questions,
